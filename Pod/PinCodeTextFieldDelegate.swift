d3d8dea5d7e80db0103de2c4461048fce40ce245
Jakub Gert <jakub.gert@zattoo.com>
2023-05-11 14:06:30 +0200

    Fixed compilation warning

--- Pod/PinCodeTextFieldDelegate.swift
+++ Pod/PinCodeTextFieldDelegate.swift
@@ -8,7 +8,7 @@
 
 import Foundation
 
-public protocol PinCodeTextFieldDelegate: class {
+public protocol PinCodeTextFieldDelegate: AnyObject {
     func textFieldShouldBeginEditing(_ textField: PinCodeTextField) -> Bool // return false to disallow editing.
     
     func textFieldDidBeginEditing(_ textField: PinCodeTextField) // became first responder
